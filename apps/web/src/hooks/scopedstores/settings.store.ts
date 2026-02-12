fc76aa46eeef36d602a226bfd93a5dd1d302d670
dev0jha <ojha6773@gmail.com>
2026-02-12 22:43:21 +0530

    feat(settings): add additional fields to settings store

--- apps/web/src/hooks/scopedstores/settings.store.ts
+++ apps/web/src/hooks/scopedstores/settings.store.ts
@@ -6,6 +6,13 @@ import { createScopedAtoms } from "@/lib/state/scoped-stores"
 interface SettingAtoms {
    isSaving: Atom<boolean>
    isOpenToRecruiters: Atom<boolean>
+   /*
+   Add a form for setting page
+   */
+   name: Atom<string>
+   location: Atom<string>
+   portfolio: Atom<string>
+   bio: Atom<string>
 }
 
 /*
@@ -14,4 +21,8 @@ interface SettingAtoms {
 export const SettingStore = createScopedAtoms<SettingAtoms>(defaults => ({
    isSaving: atom<boolean>(defaults?.isSaving ?? false),
    isOpenToRecruiters: atom<boolean>(defaults?.isOpenToRecruiters ?? false),
+   name: atom<string>(defaults?.name ?? ""),
+   location: atom<string>(defaults?.location ?? ""),
+   portfolio: atom<string>(defaults?.portfolio ?? ""),
+   bio: atom<string>(defaults?.bio ?? ""),
 }))
